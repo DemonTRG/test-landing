0df9cc92efb19db99f70a409022fb114bf5944a5
DemonTRG <116480352+DemonTRG@users.noreply.github.com>
2022-11-14 19:20:43 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $('nav a[href^="#"]').click(function() {
+        let target = $(this).attr('href');
+        $('html. body').animate({
+            scrollTop: $(target).offset().top
+        },500);
+        $('nav a[href^="#"]').parent().removeClass('active');
+        $(this).parent().addClass('active');
+        $('.menu-mobile .menu').toggle(500);
+        $('.menu-burger').toggleClass('close');
+        return false;
+    });
+
+
+    $('.menu-burger').click(function() {
+        $('.menu-mobile .menu').toggle(500);
+        $(this).toggleClass('close');
+    });
+});
